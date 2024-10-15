742a4d1abad0e91aff60cb9538503e7b951c7f5b
ade-mic <ademic.aina@gmail.com>
2024-10-15 13:11:01 +0100

    1-async_comprehension.py hint type

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,7 +9,7 @@ from typing import Generator, List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[Generator]:
+async def async_comprehension() -> List[Generator[float, None, None]]:
     """
     Collects 10 random numbers from the async_generator
     using an async comprehension.
