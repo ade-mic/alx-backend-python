7d895a279ba8979f702266a88ec00eaa8099918d
ade-mic <ademic.aina@gmail.com>
2024-11-02 18:40:40 +0100

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """A test suite for github org client
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 
 from client import GithubOrgClient
@@ -32,7 +32,30 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
 
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    def test_public_repos_url(self, org_name, mock_get_json):
+        """Test GithubOrgClient._public_repos_url
+        returns the correct URL.
+        """
+        # Define the expected repos_url based on the org_name
+        mock_org_repo_url = f"https://api.github.com/orgs/{org_name}/repos"
+  
+        # Set the return value of the get_json mock
+        mock_get_json.return_value = {"repos_url": mock_org_repo_url}
+
+        client = GithubOrgClient(org_name)
+        # Patch the org property within the context of this test
+        with patch.object(GithubOrgClient, 'org',
+                           new_callable=PropertyMock) as mock_org:
+               mock_get_json.return_value = {"repos_url": mock_org_repo_url}
+            # Assert that _public_repos_url returns the correct repos_url from the payload
+        self.assertEqual(client._public_repos_url, mock_org_repo_url)
+
 
 # Run the tests
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
